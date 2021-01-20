e7203fd56f13bd8607fe0f626752520d72086734
michael-bailey <mickyb18a@gmail.com>
2021-01-20 14:05:38 +0000

    removed aeroplane route

--- src/controllers/AeroplaneController.ts
+++ src/controllers/AeroplaneController.ts
@@ -2,8 +2,4 @@ import { Router, Request, Response } from "express";
 
 const route = Router();
 
-route.route("/aeroports").get(async (req: Request, res: Response) => {
-  res.status;
-});
-
 export default route;
