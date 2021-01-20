cd6948bfe4418acc7bd7a621727155a59d02514a
michael-bailey <mickyb18a@gmail.com>
2021-01-20 14:04:54 +0000

    added swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,40 @@
 import express, { Application, Router } from "express";
 import YAML from "yamljs";
 import swaggerUi from "swagger-ui-express";
+import swaggerdocs from "swagger-jsdoc";
 
 import path from "path";
 
+import aeroports from "./data/aeroports.json";
 import AeroportController from "./controllers/AeroportController";
 
 (async () => {
-  const app: Application = express();
+  // setting up swagger docs
   const swaggerDocs = YAML.load(path.join(__dirname, "aeroports-config.yaml"));
+  let docs = swaggerdocs({
+    swaggerDefinition: swaggerDocs,
+    apis: [
+      "./index.ts",
+      "./controllers/AeroportController.ts",
+      "./models/Aeroport.ts",
+    ],
+  });
 
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  const app: Application = express();
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(docs, { explorer: true })
+  );
 
   const routes: [Router] = [AeroportController];
 
   routes.forEach((r) => {
-    app.use("/", r);
+    app.use("/v1", r);
   });
 
   app.listen(3000, () => {
